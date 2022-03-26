5facb77f838480139b505b1461cad72b33c5ba34
Angelo Talay <78316687+angelotalay@users.noreply.github.com>
2022-03-26 13:12:09 +0000

    Star scripts for alignment for each read

---
+++ Pipeline2/star_alignment_scripts/SRR1975001.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#PBS -N STAR
+#PBS -l walltime=08:00:00
+#PBS -l vmem=100gb
+#PBS -l nodes=1:ppn=16
+#PBS -m bea
+#PBS -M aagt1@student.le.ac.uk
+reference_genome=/scratch/spectre/a/aagt1/GRCh38/star/GRCh38/star
+outprefix=/scratch/spectre/a/aagt1/SteeredResearch/star_output/
+files=/scratch/spectre/a/aagt1/SteeredResearch/fastq_trimmed
+module load star/2.7.9a
+mkdir $outprefix/SRR1975001
+STAR --runThreadN 16 --genomeDir /scratch/spectre/a/aagt1/GRCh38/star --readFilesIn $files/SRR1975001_R1_paired.fastq $files/SRR1975001_R2_paired.fastq --outReadsUnmapped Fastx --outFileNamePrefix $outprefix/SRR1975001/SRR1975001 --outSAMtype BAM SortedByCoordinate
