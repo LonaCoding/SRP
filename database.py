9ad1a057cbfbbda7762570ebdb3928de286bbf99
angelotalay <angelogabrieltalay@gmail.com>
2022-04-15 17:33:03 +0100

    Backend functionality for searching the database completed.
    Displaying the gene name, log fold change and p-value is next.

--- database.py
+++ database.py
@@ -1,14 +1,26 @@
 import sqlite3 as db
 
+
 class GeneQuery2:
     def __init__(self):
         self.connection = db.connect('database/GeneQuery2.db')
 
-    def get_clusters(self):
+    def get_clusters(self) -> list:
         cursor = self.connection.cursor()
         find_clusters = cursor.execute('SELECT name from sqlite_master where type="table"')
         clusters_list = [n[0] for n in cursor.fetchall()]
         return clusters_list
 
+    def find_genes(self, cluster: str, gene: str) -> list:
+        cursor = self.connection.cursor()
+        find = cursor.execute(f'SELECT * from {cluster} WHERE genes = "{gene}"')
+        result = cursor.fetchall()
+        print(result)
+        return result
 
 
+class FoundGene:
+    def __init__(self, row: list):
+        self.name = row[0][-1]
+        self.p_value = row[0][4]
+        self.log2FC = row[0][1]
--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import render_template
+from flask import request
 import sqlite3
 import database
 
@@ -19,15 +20,32 @@ def pipeline(number):
     return render_template('analysis_pipelines.html')
 
 
-@app.route('/pipeline/<int:number>/query')
+@app.route('/pipeline/<int:number>/query', methods=['GET', 'POST'])
 def gene_query(number):
     pipeline_num = number
-    #TODO: NEED TO RE-STRUCTURE ONCE DATABASE MADE FOR PIPELINE 1
+    # TODO: NEED TO RE-STRUCTURE ONCE DATABASE MADE FOR PIPELINE 1
     if pipeline_num == 2:
         database2 = database.GeneQuery2()
         clusters = database2.get_clusters()
 
     return render_template('genequery.html', pipeline_num=pipeline_num, clusters=clusters)
+
+
+@app.route('/pipeline/<int:number>/query/search', methods=['POST'])
+def search_database(number):
+    pipeline_num = number
+    gene = request.form['gene'].upper()
+    cluster = request.form['cluster']
+    if pipeline_num == 2:
+        database2 = database.GeneQuery2()
+        find_gene = database2.find_genes(cluster, gene)
+        if len(find_gene) == 0:
+            found = f'This gene is not present in {cluster}'
+        else:
+            found = database.FoundGene(find_gene)
+            print(found.name)
+
+    return render_template('genesearch.html', pipeline_num=pipeline_num, gene=gene, cluster=cluster, found = found)
 
 
 @app.route('/references')
