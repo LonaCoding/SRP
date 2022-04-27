40c518e223162e867fa9ac5d6e6ecc46fbae348b
angelotalay <angelogabrieltalay@gmail.com>
2022-04-27 22:06:20 +0100

    Added appopriate files and filenames for the methods. Altered text processing class to get this text.

--- template_inserts.py
+++ template_inserts.py
@@ -9,9 +9,11 @@ class TemplateInsert:
     def get_text(self) -> tuple:
         figure_legends_dir = self.text_directory + '/figure_legends'
         figure_paragraphs_dir = self.text_directory + '/figure_paragraphs'
+        methods_results_dir = self.text_directory + '/methods_results'
 
         figure_legends_paths = []
         figure_paragraphs_paths = []
+        methods_results = []
 
         for file in os.listdir(figure_legends_dir):
             path = os.path.join(figure_legends_dir, file)
@@ -19,9 +21,13 @@ class TemplateInsert:
         for file in os.listdir(figure_paragraphs_dir):
             path = os.path.join(figure_paragraphs_dir, file)
             figure_paragraphs_paths.append(path)
+        for file in os.listdir(methods_results_dir):
+            path = os.path.join(methods_results_dir, file)
+            methods_results.append(path)
 
         figure_legends = []
         figure_paragraphs = []
+        methods_results_text = []
 
         for text in zip(sorted(figure_legends_paths), sorted(figure_paragraphs_paths)):
             figure_legend = text[0]
@@ -32,7 +38,13 @@ class TemplateInsert:
             with open(figure_paragraph, 'r', encoding='utf-8') as paragraph:
                 text = paragraph.read()
                 figure_paragraphs.append(text)
-        return figure_legends, figure_paragraphs
+
+        for text in methods_results_text:
+            with open(text, 'r', encoding='utf-8') as file:
+                text = file.read()
+                methods_results_text.append(text)
+
+        return figure_legends, figure_paragraphs, methods_results_text
 
     def get_images(self) -> list:
         sorted_figure_paths = sorted(os.listdir(self.images_directory))
